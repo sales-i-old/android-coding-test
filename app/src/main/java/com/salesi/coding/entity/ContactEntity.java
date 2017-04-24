0301811a6e89dc7093e8a5c31ccb12d616063df9
Janusz Hajczewski <scalaccio@wp.pl>
2017-04-24 02:04:23 +0200

    Extended ContactEntity to have all fields form data source

--- app/src/main/java/com/salesi/coding/entity/ContactEntity.java
+++ app/src/main/java/com/salesi/coding/entity/ContactEntity.java
@@ -3,8 +3,6 @@ package com.salesi.coding.entity;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-import java.util.List;
-
 /**
  * Contact Entity
  * Edit class members as required
@@ -15,7 +13,12 @@ import java.util.List;
 public class ContactEntity {
     @Expose public Integer ContactID;
     @Expose public String Title;
+    @Expose public String JobTitle;
     @Expose @SerializedName("FirstNane")
     public String FirstName;
     @Expose public String LastName;
+    @Expose public String Email;
+    @Expose public String PhoneNumber;
+    @Expose public EntityAddress Address;
+    @Expose public String[] Hobbies;
 }
