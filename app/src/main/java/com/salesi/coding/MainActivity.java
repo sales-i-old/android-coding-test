3ebfa3208668da2182f75944198b6fe13a670639
Konrad Kierus <konrad.kierus@softwarehut.com>
2017-05-09 12:50:39 +0200

    Added option to close app from the main activity

--- app/src/main/java/com/salesi/coding/MainActivity.java
+++ app/src/main/java/com/salesi/coding/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.salesi.coding.ui.adapter.TabsAdapter;
 
@@ -31,5 +33,20 @@ public class MainActivity extends AppCompatActivity {
         mTabLayout.setupWithViewPager(mViewPager);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_tab_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.close_item:
+                finish();
+                break;
+            default:
+        }
+        return true;
+    }
 }
