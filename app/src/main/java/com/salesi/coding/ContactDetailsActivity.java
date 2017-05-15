ed6fc0035c4ef5d6beb6b130e254276c6cfc2604
Szymon <Urbaczyk>
2017-05-15 15:03:14 +0200

    contacts data details layout filling layout with data

--- app/src/main/java/com/salesi/coding/ContactDetailsActivity.java
+++ app/src/main/java/com/salesi/coding/ContactDetailsActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
+import com.salesi.coding.entity.ContactAddressEntity;
 import com.salesi.coding.entity.ContactEntity;
 import com.salesi.coding.service.IContactService;
 
@@ -12,6 +14,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import dagger.Lazy;
 
@@ -24,6 +27,39 @@ public class ContactDetailsActivity extends AppCompatActivity {
     protected Lazy<IContactService> mContactService;
 
     private int contactId;
+
+    @Bind(R.id.contact_id)
+    protected TextView contact_id;
+    @Bind(R.id.title)
+    protected TextView title;
+    @Bind(R.id.first_name)
+    protected TextView first_name;
+    @Bind(R.id.last_name)
+    protected TextView last_name;
+    @Bind(R.id.job_title)
+    protected TextView job_title;
+    @Bind(R.id.phone_number)
+    protected TextView phone_number;
+    @Bind(R.id.email)
+    protected TextView email;
+    @Bind(R.id.address1)
+    protected TextView address1;
+    @Bind(R.id.address2)
+    protected TextView address2;
+    @Bind(R.id.address3)
+    protected TextView address3;
+    @Bind(R.id.address4)
+    protected TextView address4;
+    @Bind(R.id.town)
+    protected TextView town;
+    @Bind(R.id.county)
+    protected TextView county;
+    @Bind(R.id.postal_code)
+    protected TextView postal_code;
+    @Bind(R.id.country)
+    protected TextView country;
+    @Bind(R.id.hobbies)
+    protected TextView hobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +81,25 @@ public class ContactDetailsActivity extends AppCompatActivity {
         ContactEntity contactDetails;
 
         List<ContactEntity> contacts = mContactService.get().fetchContacts();
-        for (int i = 0; i < contacts.size(); i++) {
-            Log.d("SD", contacts.get(i).ContactID + "/" +contactId);
-           // if(contacts.get(contactId).ContactID.toString() == contactId)
-           // {
-              //  contactDetails = contacts.get(contactId.).ContactID;
-               // break;
-            //}
-        }//
+        contactDetails = contacts.get(contactId);
 
+        contact_id.setText(contactDetails.ContactID.toString());
+        title.setText(contactDetails.Title);
+        first_name.setText(contactDetails.FirstNane);
+        last_name.setText(contactDetails.LastName);
+        job_title.setText(contactDetails.JobTitle);
+        phone_number.setText(contactDetails.PhoneNumber);
+        email.setText(contactDetails.Email);
+        ContactAddressEntity addressEntity = contactDetails.Address;
+        address1.setText(addressEntity.Address1);
+        address2.setText(addressEntity.Address2);
+        address3.setText(addressEntity.Address3);
+        address4.setText(addressEntity.Address4);
+        town.setText(addressEntity.Town);
+        county.setText(addressEntity.County);
+        postal_code.setText(addressEntity.Postcode);
+        country.setText(addressEntity.Country);
+        hobbies.setText(contactDetails.Hobbies.toString());
 
     }
 }
