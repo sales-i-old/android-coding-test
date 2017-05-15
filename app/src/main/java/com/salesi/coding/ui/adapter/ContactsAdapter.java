c3879cd471c99a496cd338a205d92562be958849
Marcin Krolczak <mkrolczak@netizens.pl>
2017-05-15 16:16:20 +0200

    Make call

--- app/src/main/java/com/salesi/coding/ui/adapter/ContactsAdapter.java
+++ app/src/main/java/com/salesi/coding/ui/adapter/ContactsAdapter.java
@@ -2,7 +2,6 @@ package com.salesi.coding.ui.adapter;
 
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
