0bba91bdd5453987e1af7fe67b0834607f70efb5
Marcin Krolczak <mkrolczak@netizens.pl>
2017-05-15 12:22:22 +0200

    - Fetch contact - implementation

--- app/src/main/java/com/salesi/coding/mapper/IContactMapper.java
+++ app/src/main/java/com/salesi/coding/mapper/IContactMapper.java
@@ -1,5 +1,7 @@
 package com.salesi.coding.mapper;
 
+import android.support.annotation.Nullable;
+
 import com.salesi.coding.entity.ContactEntity;
 
 import java.util.List;
@@ -10,6 +12,9 @@ import java.util.List;
  * Copyright © 2017 sales­i
  */
 public interface IContactMapper {
+
     List<ContactEntity> fetchContacts();
+
+    @Nullable
     ContactEntity fetchContact(Integer ContactID);
 }
--- app/src/main/java/com/salesi/coding/mapper/impl/ContactMapperImpl.java
+++ app/src/main/java/com/salesi/coding/mapper/impl/ContactMapperImpl.java
@@ -1,10 +1,13 @@
 package com.salesi.coding.mapper.impl;
 
+import android.support.annotation.Nullable;
+
 import com.salesi.coding.entity.ContactEntity;
 import com.salesi.coding.mapper.IContactMapper;
 import com.salesi.coding.mapper.adapter.ContactsAPI;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,8 +22,11 @@ import retrofit2.Response;
 public class ContactMapperImpl implements IContactMapper {
     private final ContactsAPI mContactsAPI;
 
+    private List<ContactEntity> contacts = null;
+
     public ContactMapperImpl(ContactsAPI contactsAPI) {
         mContactsAPI = contactsAPI;
+        contacts = new ArrayList<>();
     }
 
     @Override
@@ -28,15 +34,31 @@ public class ContactMapperImpl implements IContactMapper {
         Call<List<ContactEntity>> call = mContactsAPI.fetchContacts();
         try {
             Response<List<ContactEntity>> response = call.execute();
-            return response.body();
+            contacts = response.body();
+            return contacts;
         } catch (IOException e) {
             e.printStackTrace();
         }
         return Collections.EMPTY_LIST;
     }
 
+    @Nullable
     @Override
     public ContactEntity fetchContact(Integer ContactID) {
-        return null;
+        fetchContactsIfNecessary();
+        ContactEntity contactFound = null;
+        for (ContactEntity contact : contacts) {
+            if (contact.ContactID.equals(ContactID)) {
+                contactFound = contact;
+                break;
+            }
+        }
+        return contactFound;
+    }
+
+    private void fetchContactsIfNecessary() {
+        if (contacts.size() == 0) {
+            fetchContacts();
+        }
     }
 }
