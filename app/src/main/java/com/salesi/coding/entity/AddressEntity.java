ddf3f46a6806e0b7cf1dc879d62689e3811d11a3
SrujanaMattapalli <srujana.mattapally@gmail.com>
2017-09-10 20:16:35 +0100

    final changes made, ready for review

---
+++ app/src/main/java/com/salesi/coding/entity/AddressEntity.java
@@ -0,0 +1,18 @@
+package com.salesi.coding.entity;
+
+import com.google.gson.annotations.Expose;
+
+/**
+ * Address Entity
+ */
+
+public class AddressEntity {
+    @Expose public String Address1;
+    @Expose public String Address2;
+    @Expose public String Address3;
+    @Expose public String Address4;
+    @Expose public String Town;
+    @Expose public String County;
+    @Expose public String Postcode;
+    @Expose public String Country;
+}
